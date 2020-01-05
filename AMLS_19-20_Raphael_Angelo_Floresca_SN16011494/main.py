119e18cbbbad4aaa0c8f225a6eb6a8747add7416
raphaelfloresca <floresca.raph@gmail.com>
2020-01-05 00:45:03 +0000

    Attempted to put model training inside the mlp.py module, may allow for required format in main.py

--- AMLS_19-20_Raphael_Angelo_Floresca_SN16011494/main.py
+++ AMLS_19-20_Raphael_Angelo_Floresca_SN16011494/main.py
@@ -6,8 +6,8 @@ from A1.a1 import A1
 # ======================================================================================================================
 # Task A1
 model_A1 = A1()                                     # Build model object.
-acc_A1_train = model_A1.mlp_train(10)[0]            # Train model based on the training set (you should fine-tune your model based on validation set.)
-acc_A1_test = model_A1.mlp_test(acc_A1_train[1])    # Test model based on the test set.
+acc_A1_train = model_A1.mlp_train()                 # Train model based on the training set (you should fine-tune your model based on validation set.)
+acc_A1_test = model_A1.mlp_test()                  # Test model based on the test set.
 
 # ======================================================================================================================
 # Task A2
@@ -35,6 +35,7 @@ acc_A1_test = model_A1.mlp_test(acc_A1_train[1])    # Test model based on the te
 
 # ======================================================================================================================
 ## Print out your results with following format:
+print('TA1:{},{}'.format(acc_A1_train, acc_A1_test))
 # print('TA1:{},{};TA2:{},{};TB1:{},{};TB2:{},{};'.format(acc_A1_train, acc_A1_test,
 #                                                        acc_A2_train, acc_A2_test,
 #                                                        acc_B1_train, acc_B1_test,
--- AMLS_19-20_Raphael_Angelo_Floresca_SN16011494/pipeline/models/mlp.py
+++ AMLS_19-20_Raphael_Angelo_Floresca_SN16011494/pipeline/models/mlp.py
@@ -5,18 +5,43 @@
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Flatten
 
-class MLP:
-    @staticmethod
-    def build(height, width, num_classes, first_af, second_af, layer1_hn, layer2_hn):
-        # Instantiate Sequential API
-        model = Sequential([
-            # This flattens the 218x178x3 input into a 1D tensor
-            Flatten(input_shape=(height,width,3)),
-            # Fully connected layer with 300 neurons, using ReLU by default.
-            Dense(layer1_hn, activation=first_af),
-            # Fully connected layer with 100 neurons, using ReLU by default.
-            Dense(layer2_hn, activation=second_af),
-            # Output layer
-            Dense(num_classes, activation="softmax") 
-        ])
-        return model
+def train_mlp(
+        height, 
+        width, 
+        num_classes, 
+        epochs,
+        batch_size, 
+        train_gen, 
+        val_gen, 
+        first_af, 
+        second_af, 
+        layer1_hn, 
+        layer2_hn):
+    
+    # Instantiate Sequential API
+    model = Sequential([
+        # This flattens the 218x178x3 input into a 1D tensor
+        Flatten(input_shape=(height,width,3)),
+        # Fully connected layer with 300 neurons, using ReLU by default.
+        Dense(layer1_hn, activation=first_af),
+        # Fully connected layer with 100 neurons, using ReLU by default.
+        Dense(layer2_hn, activation=second_af),
+        # Output layer
+        Dense(num_classes, activation="softmax") 
+    ])
+
+    # We now compile the MLP model to specify the loss function
+    # and the optimizer to use (SGD)
+    model.compile(
+        loss="sparse_categorical_crossentropy", # b/c of exclusive, sparse outputs
+        optimizer='sgd', # We use SGD to optimise the ANN
+        metrics=["accuracy"]) # Used for classifiers
+
+    # Training and evaluating the MLP model on the gender dataset
+    history = model.fit(
+        train_gen,
+        steps_per_epoch=train_gen.samples // batch_size,
+        validation_data=val_gen,
+        validation_steps=val_gen.samples // batch_size,
+        epochs=epochs)
+    return model, history

296805dc09f78e0ce9cda26845b4c6aef99fafa5
raphaelfloresca <floresca.raph@gmail.com>
2020-01-06 20:13:54 +0000

    Testing out learning rate scheduler

---
+++ AMLS_19-20_Raphael_Angelo_Floresca_SN16011494/pipeline/plotting/plotting.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from main import args
+
+# Plot the training loss and accuracy
+def plot_train_loss_acc_lr(H, schedule, tla_plot_path, lr_plot_path):
+    N = np.arange(0, args["epochs"])
+    plt.style.use("ggplot")
+    plt.figure()
+    plt.plot(N, H.history["loss"], label="train_loss")
+    plt.plot(N, H.history["val_loss"], label="val_loss")
+    plt.plot(N, H.history["acc"], label="train_acc")
+    plt.plot(N, H.history["val_acc"], label="val_acc")
+    plt.title("Training Loss and Accuracy on CIFAR-10")
+    plt.xlabel("Epoch #")
+    plt.ylabel("Loss/Accuracy")
+    plt.legend()
+    plt.savefig(tla_plot_path)
+ 
+    if schedule is not None:
+	    schedule.plot(N)
+	    plt.savefig(lr_plot_path)

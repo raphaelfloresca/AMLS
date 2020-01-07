57db2e348f656eae6b90f79898e23cb7c711e871
raphaelfloresca <floresca.raph@gmail.com>
2020-01-07 20:54:35 +0000

    Testing GradCam

--- AMLS_19-20_Raphael_Angelo_Floresca_SN16011494/pipeline/plotting/plotting.py
+++ AMLS_19-20_Raphael_Angelo_Floresca_SN16011494/pipeline/plotting/plotting.py
@@ -170,7 +170,7 @@ def plot_grad_cam(model, X_test, y_test, top_n, layer_name, grad_cam_plot_path):
     top_n_data = create_top_n_data(model, X_test, y_test, top_n)
 
     plt.subplots(nrows=top_n, ncols=3)
-    plt.figure(figsize=(24, 24))
+    plt.figure(figsize=(12, 12))
 
     for i in range(top_n):
         img = X_test[int(top_n_data[i,0])] 

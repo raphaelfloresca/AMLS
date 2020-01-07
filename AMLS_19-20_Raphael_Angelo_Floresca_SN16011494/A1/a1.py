b924fba1a790cd64914606f99919417208d973f7
raphaelfloresca <floresca.raph@gmail.com>
2020-01-07 12:11:18 +0000

    Testing OOP

--- AMLS_19-20_Raphael_Angelo_Floresca_SN16011494/A1/a1.py
+++ AMLS_19-20_Raphael_Angelo_Floresca_SN16011494/A1/a1.py
@@ -27,13 +27,13 @@ class A1_MLP(A1):
             epochs,
             learning_rate,
             schedule,
-            random_state=42,
+            random_state,
             first_af="relu",
             second_af="relu",
             layer1_hn=300,
             layer2_hn=100):
 
-        # Change random state according to function
+        # Change random state according to constructor
         self.random_state = random_state
         A1.random_state = self.random_state
 
@@ -46,7 +46,7 @@ class A1_MLP(A1):
             A1.width,
             A1.num_classes,
             A1.batch_size,
-            epochs,
+            self.epochs,
             learning_rate,
             schedule,
             self.train_gen,
@@ -85,6 +85,73 @@ class A1_MLP(A1):
 
         # Plot top losses
         plot_top_losses(self.model, gender_X_test, gender_y_test, "output/plot_top_losses_A1_mlp.png")
+
+        # Get the test accuracy
+        test_accuracy = self.model.evaluate(gender_X_test, gender_y_test)[-1]
+        return test_accuracy
+
+class A1_CNN(A1):
+    def __init__(
+            self,
+            epochs,
+            learning_rate,
+            schedule,
+            random_state,
+            num_start_filters=16,
+            kernel_size=3,
+            fcl_size=512):
+
+        # Change random state according to constructor
+        self.random_state = random_state
+        A1.random_state = self.random_state
+
+        self.epochs = epochs
+
+        self.train_gen, self.val_gen, self.test_gen = A1.train_gen, A1.val_gen, A1.test_gen
+
+        self.model, self.history, self.schedule = train_cnn(
+            A1.height, 
+            A1.width,
+            A1.num_classes,
+            A1.batch_size,
+            self.epochs,
+            learning_rate,
+            schedule,
+            self.train_gen,
+            self.val_gen,
+            num_start_filters,
+            kernel_size,
+            fcl_size)
+
+    def train(self):
+        # Navigate to output folder in parent directory
+        go_up_three_dirs()        
+
+        # Plot training loss accuracy and learning rate change
+        plot_train_loss_acc_lr(
+            self.history,
+            self.epochs,
+            self.schedule,
+            "A1",
+            "output/train_loss_acc_A1_cnn.png",
+            "output/lr_A1_cnn.png")
+
+        # Get the training accuracy
+        training_accuracy = self.history.history['acc'][-1]
+        return training_accuracy
+        
+    def test(self):
+        # Go back to image folder
+        os.chdir("data/dataset_AMLS_19-20/celeba")
+
+        # Split ImageDataGenerator object for the test set into separate X and y test sets
+        gender_X_test, gender_y_test = get_X_y_test_sets(self.test_gen)
+
+        # Navigate to output folder in parent directory
+        go_up_three_dirs()
+
+        # Plot top losses
+        plot_top_losses(self.model, gender_X_test, gender_y_test, "output/plot_top_losses_A1_cnn.png")
 
         # Get the test accuracy
         test_accuracy = self.model.evaluate(gender_X_test, gender_y_test)[-1]

62feb8f7f88900ea19ef61bb8a18cd1b987f076c
raphaelfloresca <floresca.raph@gmail.com>
2020-01-07 12:56:29 +0000

    Testing OOP

--- AMLS_19-20_Raphael_Angelo_Floresca_SN16011494/pipeline/datasets/utilities.py
+++ AMLS_19-20_Raphael_Angelo_Floresca_SN16011494/pipeline/datasets/utilities.py
@@ -34,7 +34,7 @@ def create_celeba_df():
     os.chdir(os.path.join(data_dir, celeba_dir))
     # Import data as dataframe
     df = pd.read_csv("labels.csv", sep="\t", dtype=str)
-    go_up_two_dirs()
+    go_up_three_dirs()
     return df
 
 # Create a dataframe for the cartoon_set labels.csv
@@ -43,7 +43,7 @@ def create_cartoon_set_df():
     os.chdir(os.path.join(data_dir, cartoon_set_dir))
     # Import data as dataframe
     df = pd.read_csv("labels.csv", sep="\t", dtype=str)
-    go_up_two_dirs()
+    go_up_three_dirs()
     return df
 
 # Create ImageDataGenerators for training, validation and testing
